eb83b40b8837dcda81bc694e860b86253de01f3f
Sang Kwon <ysk5898@gmail.com>
2019-07-16 10:12:57 +0900

    20190716

---
+++ app/src/main/java/md/winitech/com/Lamda/LamdaActivity.java
@@ -0,0 +1,44 @@
+/*
+ * Create by SangKwon on 2019. 7. 10.
+ */
+
+package md.winitech.com.Lamda;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import md.winitech.com.R;
+import md.winitech.com.bindAPI.base.commActivity;
+import md.winitech.com.databinding.ActivityLamdaBinding;
+
+public class LamdaActivity extends commActivity <ActivityLamdaBinding> {
+
+    interface Compare{
+        public int compareTo(int a, int b);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setBinding(R.layout.activity_lamda);
+        getBinding().txt1.setText("초기상태");
+        getBinding().btn1.setOnClickListener(onClickListener);
+    }
+
+    /**
+     * 리스너를 람다식으로 변환하여 구현
+     * */
+    View.OnClickListener onClickListener = (v)->{
+
+        /**
+         * 인터페이스를 생성 후 해당 인터페이스를 다른곳에서 사용 할 때 간단하게 구현하기 위해 람다식 사용
+         * */
+        test((a,b)-> a+b, 1, 2);
+    };
+
+    public void test(Compare compare, int a, int b){
+        int value = compare.compareTo(a,b);
+        getBinding().btn1.setText(value+"");
+    }
+}
