308250d4ae0df1b38992766ed6cb86e98389d507
Sang Kwon <ysk5898@gmail.com>
2019-10-10 15:08:00 +0900

    하이브리드 앱

---
+++ app/src/main/assets/www/js/map.js
@@ -0,0 +1,12 @@
+        $(document).ready(function(){
+            var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+            console.log(varUA);
+
+            var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+            var options = { //지도를 생성할 때 필요한 기본 옵션
+                center: new kakao.maps.LatLng(35.8351122, 128.5441227), //지도의 중심좌표.
+                level: 3 //지도의 레벨(확대, 축소 정도)
+            };
+
+            var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+        });
